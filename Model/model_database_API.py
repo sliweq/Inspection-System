7d1263cd06c0fb49014ff33e3c148f4e0ac731e8
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2025-01-26 19:55:40 +0100

    Fix: model_database_api failed import

--- Model/model_database_API.py
+++ Model/model_database_API.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from .models_pydantic import *
-from .models_sqlalchemy import *
+from models_pydantic import *
+from models_sqlalchemy import *
 from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.orm import aliased, declarative_base, sessionmaker
