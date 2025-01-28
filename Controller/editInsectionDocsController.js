a56269230768c16cf02fa3596dd9d25af43c1a2a
Jakub Śliwka <kubapplll@gmail.com>
2025-01-28 14:41:28 +0100

    Added semicolon

--- Controller/editInsectionDocsController.js
+++ Controller/editInsectionDocsController.js
@@ -4,15 +4,15 @@ import {
     checkNumberInput,
     createPopup,
     sortByDate,
-} from './utils.js'
+} from './utils.js';
 
-import { fetchData } from './controllerUtils.js'
+import { fetchData } from './controllerUtils.js';
 
-document.addEventListener('DOMContentLoaded', loadDocs)
-document.getElementById('buttonSave').addEventListener('click', saveDocs)
-document.getElementById('buttonCancel').addEventListener('click', cancelDocs)
-document.getElementById('searchInput').addEventListener('input', filterByName)
-document.getElementById('filterSelect').onchange = sortByDate
+document.addEventListener('DOMContentLoaded', loadDocs);
+document.getElementById('buttonSave').addEventListener('click', saveDocs);
+document.getElementById('buttonCancel').addEventListener('click', cancelDocs);
+document.getElementById('searchInput').addEventListener('input', filterByName);
+document.getElementById('filterSelect').onchange = sortByDate;
 
 /**
  * Asynchronously loads inspection documents and appends them to the item list in the DOM.
@@ -25,19 +25,19 @@ document.getElementById('filterSelect').onchange = sortByDate
  * @throws Will throw an error if the documents cannot be loaded.
  */
 async function loadDocs() {
-    const itemList = document.getElementById('itemList')
+    const itemList = document.getElementById('itemList');
 
     try {
-        const docs = await fetchData('http://localhost:5000/inspection-docs/')
+        const docs = await fetchData('http://localhost:5000/inspection-docs/');
         console.log(docs);
         docs.forEach((doc) => {
-            const listItem = createListItem(doc)
-            itemList.appendChild(listItem)
-        })
+            const listItem = createListItem(doc);
+            itemList.appendChild(listItem);
+        });
 
-        sortByDate()
+        sortByDate();
     } catch (error) {
-        console.error('Error loading documents:', error)
+        console.error('Error loading documents:', error);
     }
 }
 
@@ -52,9 +52,9 @@ async function loadDocs() {
  * @returns {HTMLElement} The list item element representing the document.
  */
 function createListItem(doc) {
-    const listItem = document.createElement('li')
-    listItem.setAttribute('value', doc.date)
-    listItem.setAttribute('name', 'tmp')
+    const listItem = document.createElement('li');
+    listItem.setAttribute('value', doc.date);
+    listItem.setAttribute('name', 'tmp');
     listItem.innerHTML = `
         <div class="inner_list_div" id="document_${doc.id}">
             <span>${fixStringDate(doc.date)} ${Array.from(doc.subject_type)[0]} ${doc.subject} ${doc.teacher}</span>
@@ -62,12 +62,12 @@ function createListItem(doc) {
                 <img src="images/edit.png" alt="Edit button" class="trash_img">
             </button>
         </div>
-    `
+    `;
 
-    const deleteButton = listItem.querySelector('.inner_list_div button img')
-    deleteButton.addEventListener('click', () => editItem(doc.id))
+    const deleteButton = listItem.querySelector('.inner_list_div button img');
+    deleteButton.addEventListener('click', () => editItem(doc.id));
 
-    return listItem
+    return listItem;
 }
 
 /**
@@ -85,24 +85,24 @@ function createListItem(doc) {
  * @returns {void}
  */
 async function saveDocs() {
-    const inputs = getInputs()
-    const invalidFields = validateInputs(inputs)
+    const inputs = getInputs();
+    const invalidFields = validateInputs(inputs);
 
     if (invalidFields.length > 0) {
         createPopup(`Invalid input data!\n${invalidFields.join('\n')}`, [
             { text: 'Ok', color: 'ok_popup_btn' },
-        ])
-        return
+        ]);
+        return;
     }
 
-    const editableDiv = document.getElementById('editable')
+    const editableDiv = document.getElementById('editable');
     createPopup('Do you want to save document?', [
         {
             text: 'Yes',
             color: 'save_popup_btn',
             onClick: () => {
-                editableDiv.classList.add('hidden')
-                resetInputsErrors()
+                editableDiv.classList.add('hidden');
+                resetInputsErrors();
                 saveDocsChanges(editableDiv.name, {
                     lateness_minutes: inputs[0].element.value,
                     students_attendance: inputs[1].element.value,
@@ -111,11 +111,11 @@ async function saveDocs() {
                     substantive_rating: inputs[4].element.value,
                     final_rating: inputs[5].element.value,
                     objection: inputs[6].element.value,
-                })
+                });
             },
         },
         { text: 'No', color: 'cancel_popup_btn', onClick: () => {} },
-    ])
+    ]);
 }
 
 /**
@@ -174,7 +174,7 @@ function getInputs() {
             checkFunc: checkStringInput,
             label: 'Objection',
         },
-    ]
+    ];
 }
 
 /**
@@ -188,20 +188,20 @@ function getInputs() {
  * @returns {Array} - An array of error messages for invalid fields.
  */
 function validateInputs(inputs) {
-    let invalidFields = []
+    let invalidFields = [];
 
     inputs.forEach(({ element, max, checkFunc, label }) => {
-        const message = checkFunc(element.value, max)
+        const message = checkFunc(element.value, max);
 
         if (message !== true) {
-            element.classList.add('input_error')
-            invalidFields.push(`${label}: ${message}`)
+            element.classList.add('input_error');
+            invalidFields.push(`${label}: ${message}`);
         } else {
-            element.classList.remove('input_error')
+            element.classList.remove('input_error');
         }
-    })
+    });
 
-    return invalidFields
+    return invalidFields;
 }
 
 /**
@@ -210,10 +210,10 @@ function validateInputs(inputs) {
  * through each element to remove the 'input_error' class, if present.
  */
 function resetInputsErrors() {
-    const inputs = getInputs()
+    const inputs = getInputs();
     inputs.forEach(({ element }) => {
-        element.classList.remove('input_error')
-    })
+        element.classList.remove('input_error');
+    });
 }
 
 /**
@@ -248,17 +248,17 @@ async function saveDocsChanges(docsId, data) {
                     objection: data.objection,
                 }),
             }
-        )
+        );
 
         if (!response.ok) {
-            throw new Error('Failed to save document')
+            throw new Error('Failed to save document');
         }
 
         createPopup('Document saved successfully', [
             { text: 'Ok', color: 'ok_popup_btn', onClick: () => {} },
-        ])
+        ]);
     } catch (error) {
-        console.error('Error during saving document:', error)
+        console.error('Error during saving document:', error);
     }
 }
 
@@ -271,12 +271,12 @@ function cancelDocs() {
             text: 'Yes',
             color: 'save_popup_btn',
             onClick: () => {
-                document.getElementById('editable').classList.add('hidden')
-                resetInputsErrors()
+                document.getElementById('editable').classList.add('hidden');
+                resetInputsErrors();
             },
         },
         { text: 'No', color: 'cancel_popup_btn', onClick: () => {} },
-    ])
+    ]);
 }
 
 /**
@@ -290,13 +290,13 @@ function cancelDocs() {
 function filterByName() {
     const searchValue = document
         .getElementById('searchInput')
-        .value.toLowerCase()
-    const items = document.querySelectorAll('#itemList li')
+        .value.toLowerCase();
+    const items = document.querySelectorAll('#itemList li');
 
     items.forEach((item) => {
-        const text = item.textContent.toLowerCase()
-        item.style.display = text.includes(searchValue) ? 'block' : 'none'
-    })
+        const text = item.textContent.toLowerCase();
+        item.style.display = text.includes(searchValue) ? 'block' : 'none';
+    });
 }
 
 /**
@@ -310,19 +310,19 @@ function filterByName() {
  * @returns {Promise<void>} - A promise that resolves when the item has been edited.
  */
 async function editItem(id) {
-    const editableDiv = document.getElementById('editable')
-    editableDiv.classList.remove('hidden')
-    editableDiv.name = id
+    const editableDiv = document.getElementById('editable');
+    editableDiv.classList.remove('hidden');
+    editableDiv.name = id;
 
     try {
-        const docDetails = await fetchDocDetails(id)
+        const docDetails = await fetchDocDetails(id);
 
         if (docDetails) {
-            setDocDetails(docDetails)
-            editableDiv.classList.remove('hidden')
+            setDocDetails(docDetails);
+            editableDiv.classList.remove('hidden');
         }
     } catch (error) {
-        console.error('Cannot load document details:', error)
+        console.error('Cannot load document details:', error);
     }
 }
 
@@ -348,41 +348,41 @@ async function editItem(id) {
  */
 function setDocDetails(docDetails) {
     document.getElementById('Inspected_name').innerHTML =
-        docDetails.inspected_name
+        docDetails.inspected_name;
     document.getElementById('Inspected_department').innerHTML =
-        docDetails.department_name
+        docDetails.department_name;
     document.getElementById('Inspection_date').innerHTML = fixStringDate(
         docDetails.date_of_inspection
-    )
+    );
     document.getElementById('Inspected_Subject').innerHTML =
-        docDetails.subject_name
+        docDetails.subject_name;
     document.getElementById('Inspected_Subject_code').innerHTML =
-        docDetails.subject_code
+        docDetails.subject_code;
     document.getElementById('Inspectors').innerHTML = docDetails.inspectors
         .map((i) => `${i.title} ${i.name} ${i.surname}`)
-        .join(', ')
-    console.log(docDetails.inspectors)
+        .join(', ');
+    console.log(docDetails.inspectors);
     document.getElementById('Inspected_lateness_input').value = Number(
         docDetails.lateness_minutes
-    )
+    );
     document.getElementById('Student_attendance_input').value = Number(
         docDetails.student_attendance
-    )
+    );
     document.getElementById('Room_adaptation_input').value = String(
         docDetails.room_adaptation
-    )
+    );
     document.getElementById('Content_compatibility_input').value = Number(
         docDetails.content_compatibility
-    )
+    );
     document.getElementById('Substantive_assessment_input').value = String(
         docDetails.substantive_rating
-    )
+    );
     document.getElementById('Final_assessment_input').value = Number(
         docDetails.final_rating
-    )
+    );
     document.getElementById('Recommendation_input').value = String(
         docDetails.objection
-    )
+    );
 }
 
 /**
@@ -393,9 +393,9 @@ function setDocDetails(docDetails) {
  */
 async function fetchDocDetails(id) {
     try {
-        return await fetchData(`http://localhost:5000/inspection-docs/${id}/`)
+        return await fetchData(`http://localhost:5000/inspection-docs/${id}/`);
     } catch (error) {
-        console.error('Error fetching document details:', error)
-        return null
+        console.error('Error fetching document details:', error);
+        return null;
     }
 }
